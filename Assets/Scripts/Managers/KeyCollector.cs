da912e56661d1bb941e8d53b9800792e87871cf1
Star Rocha <44957149+starrocha@users.noreply.github.com>
2026-05-02 10:41:58 -0400

    Key system testing

---
+++ Assets/Scripts/Managers/KeyCollector.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class KeyCollector : MonoBehaviour
+{
+    [SerializeField] public int keyID;
+    private KeyManager keyManager;
+    private bool keyState;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        // check to see if we collected this key already
+        keyState = keyManager.GetKeysHeld()[keyID];
+
+        if (keyState == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // check if we are in contact with a goal item
+        if (collision.CompareTag("Player") )
+        {
+             // remove the key itself
+            gameObject.SetActive(false);
+
+            Debug.Log("Key " + keyID + " picked up");
+
+            // play the gate opening sound
+            soundManager.Instance.PlaySFX("gateopen");
+
+            // update keys held
+            keyManager.SetKeysHeld(keyID, true);
+
+           
+        }
+    }
+}
--- Assets/Scripts/Managers/KeyManager.cs
+++ Assets/Scripts/Managers/KeyManager.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class KeyManager : MonoBehaviour
 {
     // set up key data
-    private KeyData keyData = new KeyData();
+    private static KeyData keyData = new KeyData();
+    // private GameObject[] keyObjects = new GameObject[4];
+    [SerializeField] public GameObject[] keyObjects = new GameObject[4];
+    [SerializeField] public GameObject[] hiddenKeys = new GameObject[4];
 
     void Start()
     {
         // init keys held
         keyData.keysHeld = new bool[4] {false, false, false, false};
+
+        for (int i = 0; i < 4; i++)
+        {
+            keyObjects[i].SetActive(false);
+        }
     }
 
     public bool[] GetKeysHeld()
@@ -19,9 +29,15 @@ public class KeyManager : MonoBehaviour
     public void SetKeysHeld(int id, bool value)
     {
         // check array bounds
-        if (id >= 0 || id <= 3)
+        if (id >= 0 && id <= 3)
         {
+            Debug.Log("Set key " + id + " to " + value);
+
+            // update keydata
             keyData.keysHeld[id] = value;
+            // swap hidden and new key values
+            hiddenKeys[id].SetActive(!value);
+            keyObjects[id].SetActive(value);
         }
         else
         {
