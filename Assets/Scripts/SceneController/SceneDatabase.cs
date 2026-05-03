31abc2d6ac14854bf7fee544226b7cee4651c309
Star Rocha <44957149+starrocha@users.noreply.github.com>
2026-05-03 19:57:02 -0400

    Added end cutscene draft

--- Assets/Scripts/SceneController/SceneDatabase.cs
+++ Assets/Scripts/SceneController/SceneDatabase.cs
@@ -16,9 +16,12 @@ public class SceneDatabase : MonoBehaviour
         public const string TutorialContinued = "TutorialContinued";
         public const string Central = "Central";
         public const string Area1 = "Area1";
+        public const string Treasure1 = "Treasure1";
         public const string Area2 = "Area2";
         public const string Area3 = "Area3";
         public const string Area4 = "Area4";
         public const string SettingsWindow = "SettingsWindow";
+        public const string TheEnd = "TheEnd";
+        public const string EndTitle = "EndTitle";
     }
 }
